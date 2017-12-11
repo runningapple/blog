e0d1221af6604809f61a255b0a52762175a99341
runningapple <runningapple8@gmail.com>
2017-12-11 19:39:31 +0800

    add code

---
+++ LeetCode/24.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode result = new ListNode(0);
+        result.next = head;
+        ListNode current = result;
+        while (current.next != null && current.next.next != null) {
+            ListNode first = current.next;
+            ListNode second = current.next.next;
+            first.next = second.next;
+            current.next = second;
+            current.next.next = first;
+            current = current.next.next;
+        }
+        return result.next;
+    }
+}
